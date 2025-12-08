0765b3ce30b1bf09bab784cfc046b27ad0225dbf
Roxy <149881577+Rox-Codelabs@users.noreply.github.com>
2025-12-08 15:24:20 +0100

    css en js update

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-/*
-   De eerste interactie is al voor jullie uitgewerkt
-   Als je op het 12e linkje klikt (“Interaction”), springt deze omhoog
-*/
+/* De eerste interactie is al voor jullie uitgewerkt
+   Als je op het 2e linkje klikt (Design), springt deze omhoog */
 
-// Stap 1: selecteer het 12e linkje, en sla deze op in een variabele
-let interaction = document.querySelector('a[href="#interaction"]')
+// Stap 1: selecteer het Design linkje, en sla deze op in een variabele
+let designLink = document.querySelector('a[href="#design"]')
 
 // Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
 // In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
-interaction.addEventListener('click', jumpHandler)
+designLink.addEventListener('click', jumpHandler)
 
 // Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
 // _niet_ meteen bij het laden van de pagina aangeroepen
 function jumpHandler() {
-
   // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
-  interaction.classList.toggle('jump')
+  designLink.classList.toggle('jump')
 }
 
 // Extraatje, waardoor de class weer weggehaald wordt zodra de animatie afgelopen is
-interaction.addEventListener('animationend', jumpHandler)
-
+// Dit is trouwens geen UIEvent, maar een AnimationEvent
+designLink.addEventListener('animationend', jumpHandler)
 
 
 // ---------------------------------------------------------------------------------------------------------------Frontend interactie click -> scale
